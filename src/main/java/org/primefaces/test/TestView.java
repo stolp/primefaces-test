c48741323de9aaacc9f78442e47174e2f84c0e76
Torsten Stolpmann <stolp@verit.de>
2020-06-21 19:51:18 +0200

    First attempt.

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,19 @@
+/*
+ * Copyright 2020 verit Informationssysteme GmbH, Europaallee 10,
+ * 67657 Kaiserslautern, Germany, http://www.verit.de.
+ * 
+ * All rights reserved.
+ * 
+ * This product or document is protected by copyright and distributed
+ * under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or documentation may be
+ * reproduced in any form by any means without prior written authorization
+ * of verit Informationssysteme GmbH and its licensors, if any.
+ */
 package org.primefaces.test;
 
 import java.io.Serializable;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -10,6 +23,7 @@ import javax.inject.Named;
 public class TestView implements Serializable {
     
     private String testString;
+    private boolean disabled;
     
     @PostConstruct  
     public void init() {
@@ -22,5 +36,26 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
+    
+    public void action() {
+        
+        setDisabled(!isDisabled());
+    }
+
+    public void enableAction() {
+        
+        setDisabled(false);
+    }
+
+    public boolean isDisabled() {
+
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+
+        this.disabled = disabled;
+    }
+    
 }
