650f5c4f6d66ae742f9dceac16fbd4078c7fac29
Torsten Stolpmann <stolp@verit.de>
2021-01-13 14:19:22 +0100

    Innitial version to prove #6805.

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2021 verit Informationssysteme GmbH, Europaallee 10,
+ * 67657 Kaiserslautern, Germany, http://www.verit.de.
+ * 
+ * All rights reserved.
+ * 
+ * This product or document is protected by copyright and distributed
+ * under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or documentation may be
+ * reproduced in any form by any means without prior written authorization
+ * of verit Informationssysteme GmbH and its licensors, if any.
+ */
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.Date;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -10,6 +24,7 @@ import javax.faces.bean.ViewScoped;
 public class TestView implements Serializable {
     
     private String testString;
+    private Date testDate;
     
     @PostConstruct  
     public void init() {
@@ -22,5 +37,13 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
+    }   
+    
+    public Date getTestDate() {
+        return testDate;
+    }
+
+    public void setTestDate(Date testDate) {
+        this.testDate = testDate;
     }    
 }
