9aa4de7350b10a5050fb7302dd2c6c3c74f1e45e
Torsten Stolpmann <stolp@verit.de>
2024-11-06 13:38:19 +0100

    Initial version.

--- src/main/java/org/primefaces/test/TestObject.java
+++ src/main/java/org/primefaces/test/TestObject.java
@@ -1,13 +1,62 @@
+/*
+ * Copyright 2024 verit Informationssysteme GmbH, Europaallee 10,
+ * 67657 Kaiserslautern, Germany, http://www.verit.de.
+ *
+ * All rights reserved.
+ *
+ * This product or document is protected by copyright and distributed
+ * under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or documentation may be
+ * reproduced in any form by any means without prior written authorization
+ * of verit Informationssysteme GmbH and its licensors, if any.
+ */
 package org.primefaces.test;
 
 import java.io.Serializable;
 import java.util.UUID;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode
 public class TestObject implements Serializable {
+
+
+    /**
+     * @return Returns the id.
+     */
+    public String getId() {
+
+        return id;
+    }
+
+
+    /**
+     * @param id The id to set.
+     */
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+
+    /**
+     * @return Returns the name.
+     */
+    public String getName() {
+
+        return name;
+    }
+
+
+    /**
+     * @param name The name to set.
+     */
+    public void setName(String name) {
+
+        this.name = name;
+    }
 
     private String id;
     private String name;
--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,15 +1,28 @@
+/*
+ * Copyright 2024 verit Informationssysteme GmbH, Europaallee 10,
+ * 67657 Kaiserslautern, Germany, http://www.verit.de.
+ *
+ * All rights reserved.
+ *
+ * This product or document is protected by copyright and distributed
+ * under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or documentation may be
+ * reproduced in any form by any means without prior written authorization
+ * of verit Informationssysteme GmbH and its licensors, if any.
+ */
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import jakarta.annotation.PostConstruct;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.model.SelectItem;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
 import lombok.Data;
 
 @Data
@@ -22,16 +35,40 @@ public class TestView implements Serializable {
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
     private List<TestObject> list;
+    private List<TestObject> selection;
 
     @PostConstruct
     public void init() {
         string = "Welcome to PrimeFaces!!!";
         list = new ArrayList<>(Arrays.asList(
-                new TestObject("Thriller", "Michael Jackson", 1982),
+                new TestObject("", "Michael Jackson", 1982),
                 new TestObject("Back in Black", "AC/DC", 1980),
                 new TestObject("The Bodyguard", "Whitney Houston", 1992),
                 new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
         ));
+        selection = new ArrayList<>();
     }
 
+    public List<SelectItem> getSelectItems() {
+
+        List<SelectItem> selectItemsList = new ArrayList<>(list.size());
+
+        for (final TestObject item : list) {
+
+            SelectItem selectItem = new SelectItem(item.getId(), item.getName());
+            selectItemsList.add(selectItem);
+        }
+
+        return selectItemsList;
+    }
+
+    public List<TestObject> getSelection() {
+
+        return selection;
+    }
+
+    public void setSelection(List<TestObject> value) {
+
+        selection = value;
+    }
 }
